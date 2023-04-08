12306a0ff1235481978149187a1c8d769a4022f6
Code Sandbox <hello@codesandbox.io>
2023-04-08 20:21:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9fea9a597e5c9208b575fedc60df7c6e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94169f2cce80f52cac82ad9509c057b",
+    "url": "https://achinta1997.github.io/csb-623pwi/index.html"
+  },
+  {
+    "revision": "0f582bf181655e4c3e86",
+    "url": "https://achinta1997.github.io/csb-623pwi/static/js/2.aefe00b0.chunk.js"
+  },
+  {
+    "revision": "b1692b3f1c743b21eb3c",
+    "url": "https://achinta1997.github.io/csb-623pwi/static/js/main.d03c404b.chunk.js"
+  },
+  {
+    "revision": "816513699e001098b971",
+    "url": "https://achinta1997.github.io/csb-623pwi/static/js/runtime-main.160c1b7f.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://achinta1997.github.io/csb-623pwi/precache-manifest.9fea9a597e5c9208b575fedc60df7c6e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://achinta1997.github.io/csb-623pwi/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
